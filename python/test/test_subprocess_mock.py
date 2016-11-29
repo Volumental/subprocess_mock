a93435984f0d9de948c66b44ce75dbeca6147bba
vidstige <samuel.carlsson@volumental.com>
2016-11-29 14:13:28 +0100

    subprocess_mock: Adding basic `universal_newlines` support.

--- python/test/test_subprocess_mock.py
+++ python/test/test_subprocess_mock.py
@@ -15,7 +15,7 @@ def test_patch_restored():
 
 def test_check_call_success():
     with subprocess_mock.patch_subprocess() as mock:
-        mock.expect(['ls', '-l'], stdout="FOOBAR", returncode=0)
+        mock.expect(['ls', '-l'], returncode=0)
         subprocess.check_call(['ls', '-l'])
 
 
@@ -30,7 +30,7 @@ def test_check_ouput_success():
     with subprocess_mock.patch_subprocess() as mock:
         mock.expect(['ls', '-l'], stdout="FOOBAR", returncode=0)
         stdout = subprocess.check_output(['ls', '-l'])
-        assert_equal(stdout, "FOOBAR")
+        assert_equal(stdout, b'FOOBAR')
 
 
 @raises(subprocess.CalledProcessError)
@@ -53,5 +53,15 @@ def test_popen_communicate():
         mock.expect(['ls', '-l'], stdout="X", stderr="Y", returncode=0)
         p = subprocess.Popen(['ls', '-l'])
         stdout, stderr = p.communicate()
+        assert_equal(stdout, b'X')
+        assert_equal(stderr, b'Y')
+
+
+def test_popen_communicate_universal_newlines():
+    with subprocess_mock.patch_subprocess() as mock:
+        mock.expect(['ls', '-l'], stdout="X", stderr="Y", returncode=0)
+        p = subprocess.Popen(['ls', '-l'], universal_newlines=True)
+        stdout, stderr = p.communicate()
         assert_equal(stdout, "X")
         assert_equal(stderr, "Y")
+
