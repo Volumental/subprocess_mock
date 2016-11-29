8919c96994c05d36f61a412d1413fc87385f9773
vidstige <samuel.carlsson@volumental.com>
2016-11-29 14:05:10 +0100

    Refactor: Adding type hinting to `subprocess_mock`

--- python/test/utils/subprocess_mock.py
+++ python/test/utils/subprocess_mock.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 # Copyright 2016 Volumental AB. CONFIDENTIAL. DO NOT REDISTRIBUTE.
+from typing import Tuple, List, Any
 from unittest.mock import patch
 
 import subprocess
 
 
 class FakeInfo(object):
-    def __init__(self, stdout, stderr, returncode, duration):
+    def __init__(self, stdout: str, stderr: str, returncode: int, duration: int) -> None:
         self.stdout = stdout
         self.stderr = stderr
         self.returncode = returncode
@@ -16,7 +17,7 @@ class FakeInfo(object):
 class FakeProcess(object):
     def __init__(self, args, bufsize=0, executable=None, stdin=None, stdout=None, stderr=None,
                  preexec_fn=None, close_fds=False, shell=False, cwd=None, env=None,
-                 universal_newlines=False, startupinfo=None, creationflags=0):
+                 universal_newlines=False, startupinfo=None, creationflags=0) -> None:
         self.args = args
 
     def __enter__(self):
@@ -27,26 +28,30 @@ class FakeProcess(object):
 
     # NOTE: These are not to be taken as arguments to __init__ to detect problems when Popen
     # are called with wrong arguments. E.g. calling Popen with `returncode` argument must fail test
-    def _setup(self, fake_info):
+    def _setup(self, fake_info: FakeInfo):
         self.fake_info = fake_info
 
-    def communicate(self, input=None, timeout=None):
+    def communicate(self, input=None, timeout: int=None) -> Tuple[str, str]:
         return self.fake_info.stdout, self.fake_info.stderr
 
     def poll(self):
         return self.fake_info.returncode
 
-    def wait(self, timeout=None):
+    def wait(self, timeout: int=None) -> int:
         if timeout and self.fake_info.duration > timeout:
-            raise subprocess.TimeoutExpired(self.args, timeout)
+            raise subprocess.TimeoutExpired(self.args, timeout)  # type: ignore
 
         return self.fake_info.returncode
 
-    def kill(self):
+    def kill(self) -> None:
         pass
 
 
 class SubprocessMock(object):
+    def __init__(self) -> None:
+        # TODO(samuel): Actually the type is `_patch`
+        self._popen_patch = None  # type: Any
+
     def __enter__(self):
         self._popen_patch.__enter__()
         self.expected = {}
@@ -55,7 +60,7 @@ class SubprocessMock(object):
     def __exit__(self, exc_type, exc_val, exc_tb):
         return self._popen_patch.__exit__(exc_type, exc_val, exc_tb)
 
-    def Popen(self, command, *args, **kwargs):
+    def Popen(self, command: List[str], *args, **kwargs) -> FakeProcess:
         if repr(command) in self.expected:
             fake_process = FakeProcess(command, *args, **kwargs)
             fake_process._setup(self.expected[repr(command)])
@@ -65,11 +70,12 @@ class SubprocessMock(object):
         hint = "Try `mock.expect({})`".format(command)
         assert False, "{error_message}. {hint}".format(error_message=error_message, hint=hint)
 
-    def expect(self, command, stdout=None, stderr=None, returncode=0, duration=0):
+    def expect(self, command: List[str],
+               stdout: str=None, stderr: str=None, returncode: int=0, duration: int=0) -> None:
         self.expected[repr(command)] = FakeInfo(stdout, stderr, returncode, duration)
 
 
-def patch_subprocess():
+def patch_subprocess() -> SubprocessMock:
     mock = SubprocessMock()
     mock._popen_patch = patch('subprocess.Popen', mock.Popen)
     return mock
