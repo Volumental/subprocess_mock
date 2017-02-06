662cc889b4758167644d6985b3f6a8662db92be5
emilk <emil.ernerfeldt@gmail.com>
2017-02-06 16:03:36 +0100

    Update copyright for 2017

--- python/test/test_subprocess_mock.py
+++ python/test/test_subprocess_mock.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2016 Volumental AB. CONFIDENTIAL. DO NOT REDISTRIBUTE.
+# Copyright 2017 Volumental AB. CONFIDENTIAL. DO NOT REDISTRIBUTE.
 import subprocess
 from nose.tools import assert_equal, raises
 
